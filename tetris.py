a3712d7d14a85b8438e663dcd5fa8fbf47ba7429
kader <local@kader.email>
2020-10-30 06:11:12 -0400

    got the one red piece movin'

--- tetris.py
+++ tetris.py
@@ -1,11 +1,16 @@
 import pygame
 import sys
+from time import sleep
 import os
+import random
 
 size = width, height = 400, 600
 black = 0, 0, 0
 pygame.init()
 screen = pygame.display.set_mode(size)
+
+def next_color ():
+    return random.randrange(7)
 
 red = pygame.image.load(os.path.join('pieces','piece-red.png'))
 red = pygame.transform.scale (red, (250, 250))
@@ -28,20 +33,25 @@ cyan = pygame.transform.scale (cyan, (250, 250))
 green = pygame.image.load(os.path.join('pieces','piece-green.png'))
 green = pygame.transform.scale (green, (250,250))
 
-
 running = True
+x, y = 0, 0
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            red = pygame.transform.rotate(red, 90)
+        elif keys[pygame.K_DOWN] and y < 425:
+            y += 25
+        elif keys[pygame.K_LEFT] and x >= -75:
+            x -=25
+        elif keys[pygame.K_RIGHT] and x < 250:
+            x += 25
+
     screen.fill (black)
-    screen.blit(red,(0,0))
-    screen.blit(blue,(100,100))
-    screen.blit(orange,(200,150))
-    screen.blit(violet,(200,50))
-    screen.blit(yellow,(200,300))
-    screen.blit(cyan,(100,400))
-    screen.blit(green,(150, 450))
+    screen.blit(red,(x,y))
 
     pygame.display.flip()
     pygame.display.update()
