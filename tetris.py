c978b1a0d7acefbf91304e393ce1d0d157470161
kader <local@kader.email>
2020-10-30 05:05:59 -0400

    loaded all of the piece images into pygame, now making sure they all look correctly. scaling them each to have a box of 250,250. Then after that I have to do the key listeners.

--- tetris.py
+++ tetris.py
@@ -1,10 +1,33 @@
 import pygame
 import sys
+import os
 
 size = width, height = 400, 600
 black = 0, 0, 0
 pygame.init()
 screen = pygame.display.set_mode(size)
+
+red = pygame.image.load(os.path.join('pieces','piece-red.png'))
+red = pygame.transform.scale (red, (250, 250))
+
+blue = pygame.image.load(os.path.join('pieces','piece-blue.png'))
+blue = pygame.transform.scale (blue, (250, 250))
+
+orange = pygame.image.load(os.path.join('pieces','piece-orange.png'))
+orange = pygame.transform.scale (orange, (250,250))
+
+violet = pygame.image.load(os.path.join('pieces','piece-violet.png'))
+violet = pygame.transform.scale (violet, (250,250))
+
+yellow = pygame.image.load(os.path.join('pieces','piece-yellow.png'))
+yellow = pygame.transform.scale (yellow, (250, 250))
+
+cyan = pygame.image.load(os.path.join('pieces','piece-cyan.png'))
+cyan = pygame.transform.scale (cyan, (250, 250))
+
+green = pygame.image.load(os.path.join('pieces','piece-green.png'))
+green = pygame.transform.scale (green, (250,250))
+
 
 running = True
 while running:
@@ -12,5 +35,10 @@ while running:
         if event.type == pygame.QUIT:
             sys.exit()
     screen.fill (black)
+    screen.blit(red,(0,0))
+    screen.blit(blue,(100,100))
+
     pygame.display.flip()
+    pygame.display.update()
+    
     
